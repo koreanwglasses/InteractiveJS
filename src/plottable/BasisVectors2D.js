99bcda6f71b060dfeb6a816a6be7b335d41c3b05
koreanwglasses <koreanwglasses@gmail.com>
2017-06-17 23:16:20 -0400

    Changed default colors

--- src/plottable/BasisVectors2D.js
+++ src/plottable/BasisVectors2D.js
@@ -6,8 +6,8 @@ import { Arrow2D } from './Arrow2D.js';
  * @param {*} opts Options to customize the appearance of the arrows. Includes:
  * origin -- Point of the origin. Default is (0, 0, 0)
  * hex -- hexadecimal value to define color. Default is 0xffff00.
- * headLength -- The length of the head of the arrow. Default is 0.2 * length.
- * headWidth -- The length of the width of the arrow. Default is 0.2 * headLength.
+ * headLength -- The length of the head of the arrow. Default is 0.2.
+ * headWidth -- The length of the width of the arrow. Default is 0.04.
  * (Derived from THREE.js)
  */
 function BasisVectors2D(opts) {
@@ -16,8 +16,8 @@ function BasisVectors2D(opts) {
     this.xBasis = new Vector(1, 0);
     this.yBasis = new Vector(0, 1);
 
-    this.xArrow = new Arrow2D(this.xBasis, opts);   
-    this.yArrow = new Arrow2D(this.yBasis, opts);
+    this.xArrow = new Arrow2D(this.xBasis, {hex: 0x880000, headWidth: 0.04});   
+    this.yArrow = new Arrow2D(this.yBasis, {hex: 0x008800, headWidth: 0.04});
 
     this.sceneObject = null;
 }
