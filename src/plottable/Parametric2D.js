96ae882960353496b937bad7edf4ad648cdec9cb
koreanwglasses <koreanwglasses@gmail.com>
2017-06-19 12:31:40 -0400

    Added support for coloration in parametric eq

--- src/plottable/Parametric2D.js
+++ src/plottable/Parametric2D.js
@@ -3,24 +3,36 @@ import { Vector } from '../math/Vector.js';
 function Parametric2D(func, interval, opts) {
     this.func = func;
     this.interval = interval;
-    this.opts = opts;
+    this.opts = opts !== undefined? opts: {}
 
     this.validated = false;
     this.sceneObject = null;
+
+    this.color = this.opts.color;
 }
 
 Parametric2D.prototype.createLine = function() {
     var geom = new THREE.Geometry();
     var tarr = this.interval.array();
     var X = this.func.evaluate();
+    if(this.color !== undefined) {
+        var cX = this.color.evaluate();
+    }
     var t = new Vector(0);
     for(var i = 0; i < tarr.length; i++) {
         t.q[0] = tarr[i]
         geom.vertices.push(X(t).toVector3());
+
+        if(this.color !== undefined) {
+            var color = cX(t);
+            geom.colors[i] = new THREE.Color(color.q[0], color.q[1], color.q[2])
+        }
+    } 
+    if(this.color !== undefined) {
+        return new THREE.Line(geom, new THREE.LineBasicMaterial({color: 0xffffff, vertexColors: THREE.VertexColors}));
+    } else {
+        return new THREE.Line(geom, new THREE.LineBasicMaterial());
     }
-    // geom.colors[0] = new THREE.Color(1,0,0);
-    // {color: 0xffffff, vertexColors: THREE.VertexColors}
-    return new THREE.Line(geom, new THREE.LineBasicMaterial());
 }
 
 Parametric2D.prototype.getSceneObject = function() {
