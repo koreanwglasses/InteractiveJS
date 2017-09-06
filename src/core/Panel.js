ba8d23d4463c7762dcf2826ae0a7b28378c09ff2
koreanwglasses <koreanwglasses@gmail.com>
2017-09-06 19:11:39 -0400

    Added value output to slider

--- src/core/Panel.js
+++ src/core/Panel.js
@@ -22,22 +22,29 @@ Panel.prototype.addSlider = function(expr, opts) {
         slider.value = this.parent.context[interval.varstr].value;
     }
 
+    var valueLabel = document.createTextNode(slider.value);
+
     var _self = this;
     if(opts.continuous === undefined || opts.continuous === false) {
         slider.onchange = function() {            
             _self.parent.context[interval.varstr] = new Number(parseFloat(slider.value));
             _self.parent.refresh(interval.varstr);
+            
+            valueLabel.nodeValue = slider.value;
         }
     } else if(opts.continuous === true) {
         slider.oninput = function() {
             _self.parent.context[interval.varstr] = new Number(parseFloat(slider.value));
             _self.parent.refresh(interval.varstr);
+
+            valueLabel.nodeValue = slider.value;
         }
     }
 
-    var label = document.createTextNode(interval.varstr + ' = ');
+    var label = document.createTextNode(interval.varstr + ':');
     this.container.appendChild(label);
     this.container.appendChild(slider);
+    this.container.appendChild(valueLabel);
 }
 
 export { Panel };
