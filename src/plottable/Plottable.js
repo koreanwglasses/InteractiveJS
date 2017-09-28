e0f757f07853fca0b4f304e0b148f4bf8b1d6333
koreanwglasses <koreanwglasses@gmail.com>
2017-09-28 15:16:45 -0400

    Added option to hide plottables dynamically

--- src/plottable/Plottable.js
+++ src/plottable/Plottable.js
@@ -14,11 +14,17 @@ function Plottable(plot, expr, opts) {
     
     this.sceneObject = null;
     this.validated = false;
+
+    if(opts === undefined) opts = {}
+    this.showExpr = opts.show !== undefined ? new Expression(opts.show, plot.context) : null;
 }
 
 Plottable.prototype.getVariables = function() {
     if(!this.expr) {
         return []
+    }
+    if(this.showExpr) {
+        return this.expr.getVariables().concat(this.showExpr.getVariables());
     }
     return this.expr.getVariables();
 }
@@ -28,7 +34,11 @@ Plottable.prototype.getVariables = function() {
  */
 Plottable.prototype.getSceneObject = function() {
     if(this.validated === false) {
-        this.sceneObject = this.createSceneObject();
+        if(!this.showExpr || this.showExpr.evaluate() != 0) {
+            this.sceneObject = this.createSceneObject();
+        } else {
+            this.sceneObject = null;
+        }
         this.validated = true;
     }
     return this.sceneObject;
