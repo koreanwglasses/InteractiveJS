44bfce7fc41d40adaf2fc6e57531eb038a141d31
choif <choif@rpi.edu>
2018-11-16 01:23:44 -0500

    Nuked original code

--- rollup.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    input: 'src/Interactive.js',
-    output: {
-        name: 'Interactive',
-        format: 'umd',
-        file: 'build/interactive.js'
-    }
-}
--- src/utils/uid.js
+++
@@ -1,7 +0,0 @@
-var uid = {};
-uid.counter = 0;
-uid.getUid = function() {
-    return uid.counter++;
-}
-
-export { uid };
