0aaab027d5c5d258681d1de7938f1918e197a687
choif <choif@rpi.edu>
2018-11-16 01:29:38 -0500

    Added compiler options

---
+++ build/Interactive.js
@@ -0,0 +1,26 @@
+System.register("context", [], function (exports_1, context_1) {
+    "use strict";
+    var Context;
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [],
+        execute: function () {
+            Context = (function () {
+                function Context() {
+                }
+                return Context;
+            }());
+            exports_1("Context", Context);
+        }
+    };
+});
+System.register("expression", [], function (exports_2, context_2) {
+    "use strict";
+    var __moduleName = context_2 && context_2.id;
+    return {
+        setters: [],
+        execute: function () {
+        }
+    };
+});
+//# sourceMappingURL=Interactive.js.map
