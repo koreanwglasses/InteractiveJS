c8095a49922456bbb7648b0e6e0d32fb4d5ae4bf
choif <choif@rpi.edu>
2018-11-26 11:40:15 -0500

    Added uid to figure for hashing

--- src/plot/Axes.ts
+++ src/plot/Axes.ts
@@ -1,19 +1,39 @@
 import { Figure } from "./Figure";
 import { Plot } from "./Plot";
+import * as THREE from "three";
+import { instanceOf } from "prop-types";
 
 /**
  * Used for plotting. Can put multiple figures on axes.
  */
 export abstract class Axes {
     private plot: Plot;
+    private container: HTMLElement;
+    private width: number;
+    private height: number;
+
+    private renderer: THREE.WebGLRenderer;
+    private antialias: boolean;
+
+    private figures: Set<Figure>;
+    private meshMap: Map<Figure, THREE.Mesh>;
 
     /**
      * Creates a new Axes from given args. Throws an error if args are invalid.
      */
     public constructor(args: AxesArgs) {
         args.validate();
-
+        args.default();
         this.plot = args.plot;
+        this.container = args.container;
+        this.width = args.width;
+        this.height = args.height;
+        this.antialias = args.antialias;
+
+        this.renderer = null;
+
+        this.figures = new Set<Figure>();
+        this.meshMap = new Map<Figure, THREE.Mesh>();
 
         throw new Error("Method not implemented.")
     }
@@ -23,19 +43,37 @@ export abstract class Axes {
      * @param figure Figure to add to plot
      * @returns true if figure was not already present in this axes. false otherwise.
      */
-    public abstract addFigure(figure: Figure): boolean;
+    public addFigure(figure: Figure): boolean {
+        if(this.figures.has(figure)) {
+            return false;
+        } else {
+            this.figures.add(figure);
+            return true;
+        }
+    }
 
     /**
      * Removes the figure from this plot, if it exists. Will be erased on next call to render()
      * @param figure Figure to remove from plot
      * @returns true if figure was removed. false if it did not exist
      */
-    public abstract removeFigure(figure: Figure): boolean;
+    public removeFigure(figure: Figure): boolean {
+        if(!this.figures.has(figure)) {
+            return false;
+        } else {
+            this.figures.delete(figure);
+            return true;
+        }
+    }
 
     /**
      * Forces all figures to recalculate their scene model.
      */
-    public abstract refresh(): void;
+    public refresh(): void {
+        for(let figure of this.figures) {
+            this.meshMap.set(figure, null);
+        }
+    }
 
     /**
      * Draws all figures
@@ -45,12 +83,28 @@ export abstract class Axes {
     /**
      * Removes the GL context from the page to conserve memory.
      */
-    public abstract sleep(): void;
+    public sleep(): void {
+        if(this.renderer != null) {
+            this.renderer.forceContextLoss();
+            this.renderer.context = null;
+            this.renderer.domElement = null;        
+            this.renderer = null;
+        }
+    }
 
     /**
      * Restores the GL context.
      */
-    public abstract wake(): void;
+    public wake(): void {
+        if(this.renderer == null) {
+            this.renderer = new THREE.WebGLRenderer({antialias: this.antialias});
+    
+            // Initialize renderer within container
+            this.renderer.setSize(this.width, this.height);
+            this.container.innerHTML = '';
+            this.container.appendChild(this.renderer.domElement);
+        }
+    }
 
     /**
      * Returns the plot that this axes is created on
@@ -67,6 +121,11 @@ export abstract class Axes {
 export abstract class AxesArgs {
     public plot: Plot;
     public container: HTMLElement;
+
+    public width: number;
+    public height: number;
+
+    public antialias: boolean;
 
     constructor(args: any) {
         this.plot = args.plot;
@@ -91,5 +150,21 @@ export abstract class AxesArgs {
             throw new Error("Invalid arguments: container is not an instance of HTMLElement!");
         }
         return true;
+    }
+    
+    /**
+     * Fills in default values for undefined properties
+     */
+    public default(): void {
+        if(this.width === undefined) {
+            this.width = this.container.clientWidth;
+        }
+        if(this.height === undefined) {
+            this.height = this.container.clientHeight;
+        }
+
+        if(this.antialias === undefined) {
+            this.antialias = false;
+        }
     }
 }
--- src/plot/Figure.ts
+++ src/plot/Figure.ts
@@ -3,9 +3,24 @@ import { Mesh } from "three";
 /**
  * Represents an object that can be plotted on an axes
  */
-export interface Figure {
+export abstract class Figure {
+    private uid: number;
+
+    protected constructor() {
+        this.uid = Figure.uid_counter++;
+    }
+
     /**
      * Creates a mesh that can be rendered on a THREE.Scene
      */
-    getSceneObject: () => Mesh;
+    public abstract getSceneObject(): Mesh;
+
+    // /**
+    //  * Returns a string that uniquely identifies this figure.
+    //  */
+    // public toString(): string {
+    //     return '<Figure uid:' + this.uid + '>';
+    // }
+
+    private static uid_counter = 0;
 }
